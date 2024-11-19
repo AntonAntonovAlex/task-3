fc59dce36c17f4698440e0ea16890c74b61e9b3c
Anton Antonau <anton.antonau.mogilev@gmail.com>
2024-11-19 15:27:39 +0300

    fixes: fix random number generation

--- generatingRandomNumber.js
+++ generatingRandomNumber.js
@@ -1,10 +1,11 @@
+import { randomInt } from 'crypto';
+
 export class GeneratingRandomNumber {
     constructor(min, max) {
         this.randomNumber = this.createRandomNumber(min, max);
     };
 
     createRandomNumber(min, max) {
-        let randNum = min + Math.random() * (max + 1 - min);
-        return Math.floor(randNum);
+        return randomInt(min, max + 1);
     };
 };
